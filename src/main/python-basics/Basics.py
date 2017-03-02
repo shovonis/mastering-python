273a977f98aa37e149bce016f369c6ef5d8ecc2e
Rifatul Islam Shovon <rifatul.islam@therapservices.net>
2017-03-02 12:04:41 +0600

    Intial Commit and project setup

---
+++ src/main/python-basics/Basics.py
@@ -0,0 +1,11 @@
+_author_ = "Rifatul Islam"
+
+# In python list can contains different sets of data types.
+aList = ["This is a list", 123, 690.9, ["this is list inside list", 90], "this is outer string"]
+print(aList)
+
+aList.append("Appending a String to aList")
+print(aList)
+
+aList.extend("Extending aList")
+print(aList)
